a33271158aaef8baff02de3c01d169a963bcc9c8
shanlihang <67490009@qq.com>
2025-07-23 12:13:55 +0800

    修复路由映射

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Toaster } from 'sonner';
+import { Toaster } from "sonner";
 import App from "./App";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-    <Toaster />
+    <BrowserRouter basename="/mark-smith">
+      <App />
+      <Toaster />
+    </BrowserRouter>
   </StrictMode>
 );
