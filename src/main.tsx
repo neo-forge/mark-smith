5a24a86963c69b7fa5d0069ab6d4235ec983f096
shanlihang <67490009@qq.com>
2025-07-23 12:17:04 +0800

    设置根路径为/

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,10 @@ import { createRoot } from "react-dom/client";
 import { Toaster } from "sonner";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/mark-smith">
-      <App />
-      <Toaster />
-    </BrowserRouter>
+    <App />
+    <Toaster />
   </StrictMode>
 );
